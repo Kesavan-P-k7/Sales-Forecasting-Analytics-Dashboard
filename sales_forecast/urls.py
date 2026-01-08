b0e52c453208a26d9e3dffe9565f52d8f35d5873
Kesavan P <kesavan.pk71@gmail.com>
2026-01-08 21:42:08 +0530

    Remove type ignore comments from urls.py

--- sales_forecast/urls.py
+++ sales_forecast/urls.py
@@ -1,16 +1,15 @@
 """
 URL configuration for sales_forecast project.
 """
-from django.contrib import admin  # type: ignore
-from django.urls import path, include  # type: ignore
-from django.conf import settings  # type: ignore
-from django.conf.urls.static import static  # type: ignore
+from django.contrib import admin 
+from django.urls import path, include 
+from django.conf import settings 
+from django.conf.urls.static import static 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
 ]
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0] if settings.STATICFILES_DIRS else None)
