c76431dfd374b16f9697cab2082a0c157d9af53e
Kesavan P <kesavan.pk71@gmail.com>
2026-01-08 21:17:36 +0530

    Update admin.py

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,7 +1,5 @@
-# Django admin configuration
-from django.contrib import admin  # type: ignore
+from django.contrib import admin
 from .models import SalesData, Forecast, DataUpload
-
 
 @admin.register(SalesData)
 class SalesDataAdmin(admin.ModelAdmin):
@@ -9,15 +7,13 @@ class SalesDataAdmin(admin.ModelAdmin):
     list_filter = ('date', 'product', 'uploaded_at')
     search_fields = ('product',)
     date_hierarchy = 'date'
-
-
+    
 @admin.register(Forecast)
 class ForecastAdmin(admin.ModelAdmin):
     list_display = ('forecast_date', 'product', 'predicted_revenue', 'forecast_type', 'created_at')
     list_filter = ('forecast_type', 'forecast_date', 'created_at')
     search_fields = ('product',)
-
-
+    
 @admin.register(DataUpload)
 class DataUploadAdmin(admin.ModelAdmin):
     list_display = ('file_name', 'status', 'records_count', 'uploaded_at')
